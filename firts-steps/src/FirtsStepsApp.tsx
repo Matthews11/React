a0fc3107aa3cf925321c68f33493aebddbe75240
“Mathew <mateo12molina@gmail.com>
2026-07-02 21:55:42 -0600

    variable/props/enviando variables a componentes

--- firts-steps/src/FirtsStepsApp.tsx
+++ firts-steps/src/FirtsStepsApp.tsx
@@ -1,13 +1,13 @@
+import ItemCounter from "./shopping-cart/ItemCounter";
+
 export function FirstStepsApp() {
     return (
         <>
-            <h1>HOLA MUNDO</h1>
-            <p>Esto es un pàrrafo</p>
-            <button>Click me</button>
+            <h1>Carrito de compras</h1>
+            <ItemCounter nombre="NINTENDO SWITCH 2" cantidad={3}></ItemCounter>
+            <ItemCounter nombre="SUPER SMASH" cantidad={4}></ItemCounter>
+            <ItemCounter nombre="PRO CONTROLLER" cantidad={1}></ItemCounter>
 
-            <div>
-                <h2>PRIMER COMPONENTE REACT</h2>
-            </div>
         </>
     )
 }
